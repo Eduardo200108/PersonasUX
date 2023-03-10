68c9e7e274ec02a4886282b9964eab1590414a3f
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-03-09 20:32:41 -0600

    personasux

--- PersonasUx/src/components/Formulario.vue
+++ PersonasUx/src/components/Formulario.vue
@@ -57,12 +57,26 @@ export default {
 </script>
 
 <template>
+  
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
 
-<v-container fluid>
-      <img src="fondo.png" alt="">
-    </v-container>
+  
+  
   <div class="font-sans">
-    <div class="min-h-screen flex sm:justify-center bg-[#c4b5fd]" >
+    <div class="min-h-screen flex sm:left bg-[#c4b5fd]" >
       <div
         style="margin: 100px 10px 100px 10px"
         
@@ -220,13 +234,16 @@ export default {
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-7"
               >personalidad 1</label
             >
-            <input
+            <input 
               v-model="personalidad01"
               type="range"
               min="0"
               max="100"
-              class="mt-1 block w-full border-none  bg-yellow-200 h-11 rounded-xl shadow-lg hover:bg-yellow-200 focus:bg-yellow-200 focus:ring-0"
+              
+              class="mt-1 block w-full border-none bg-yellow-200 h-11 rounded-xl shadow-lg hover:bg-yellow-200 focus:bg-yellow-200 focus:ring-0"
+              
             />
+              
             <span>{{ this.personalidad01 }}%</span>
           </div>
 
